c62243f980ea4e283d3634158041b1f46af7f5a6
ACosmicWolf <cosmicwolf009@gmail.com>
2023-05-22 18:43:48 +0530

    responsivenessssss

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -25,30 +25,32 @@ export default async function ProfilePage() {
     <div className={styles.container}>
       <div className={styles.profileSection}>
         <div className={styles.topSection}>
-          <Image
-            src={session?.user?.image as string}
-            alt={session?.user?.name as string}
-            width={100}
-            height={100}
-          />
           <div>
-            <p>
-              {session?.user?.followers?.length}
-              <span>Followers</span>
-            </p>
-            <p>
-              {session?.user?.following?.length}
-              <span>Following</span>
-            </p>
-            <p>
-              {posts.length}
-              <span>Posts</span>
-            </p>
+            <Image
+              src={session?.user?.image as string}
+              alt={session?.user?.name as string}
+              width={100}
+              height={100}
+            />
+            <div>
+              <p>
+                {session?.user?.followers?.length}
+                <span>Followers</span>
+              </p>
+              <p>
+                {session?.user?.following?.length}
+                <span>Following</span>
+              </p>
+              <p>
+                {posts.length}
+                <span>Posts</span>
+              </p>
+            </div>
           </div>
-        </div>
-        <div className={styles.infoSection}>
-          <h1>{session?.user?.name}</h1>
-          <p>{session?.user?.email}</p>
+          <div className={styles.infoSection}>
+            <h1>{session?.user?.name}</h1>
+            <span className={styles.infoEmail}>{session?.user?.email}</span>
+          </div>
         </div>
       </div>
       <div className={styles.postsSection}>
